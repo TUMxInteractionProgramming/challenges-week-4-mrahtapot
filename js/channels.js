95e5cdbbb4df3967e5961c06254b66f6e65db432
berkeatropat <berke.atropat@tum.de>
2019-04-05 07:52:40 +0200

    Challenge 7: First Trials

---
+++ js/channels.js
@@ -0,0 +1,46 @@
+var april12016 = new Date(Date.now() - 368*864e5); // 864e5 == 86400000 == 24*60*60*1000
+
+var yummy = {
+    name: 'yummy', 
+    createdOn: april12016, 
+    createdBy: 'minus.plus.yummy', 
+    starred: false, 
+    expiresIn: 100,
+    messageCount: 999
+};
+
+var sevenContinents = {
+    name: 'sevenContinents', 
+    createdOn: april12016, 
+    createdBy: 'minus.plus.yummy', 
+    starred: true, 
+    expiresIn: 100,
+    messageCount: 999
+};
+
+var killerApp = {
+    name: 'killerApp', 
+    createdOn: april12016, 
+    createdBy: 'minus.plus.yummy', 
+    starred: false, 
+    expiresIn: 100,
+    messageCount: 999
+};
+
+var firstPersonOnMars = {
+    name: 'firstPersonOnMars', 
+    createdOn: april12016, 
+    createdBy: 'minus.plus.yummy', 
+    starred: true, 
+    expiresIn: 100,
+    messageCount: 999
+};
+
+var octoberfest = {
+    name: 'octoberfest', 
+    createdOn: april12016, 
+    createdBy: 'minus.plus.yummy', 
+    starred: false, 
+    expiresIn: 100,
+    messageCount: 999
+};
